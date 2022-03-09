d920a01465afaf706b3c24e97519774d592461c6
Polyanasousas <souzaspolyana@gmail.com>
2022-03-09 19:43:24 -0300

    adiciona data-test-id ao titulo das configuracoes

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -4,7 +4,7 @@ export default class Settings extends Component {
   render() {
     return (
       <div>
-        <h1>Configuraçoes</h1>
+        <h1 data-testid="settings-title">Configuraçoes</h1>
       </div>
     );
   }
