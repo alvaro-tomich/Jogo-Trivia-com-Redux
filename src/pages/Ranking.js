f2c5ea195b336811670030b73c2085c19b77c8f6
Kamila Hydalgo <kamilahydalgo@gmail.com>
2022-03-11 19:48:39 -0300

    Requisito 17 feito

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking jogador</h1>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Voltar ao início
+
+          </button>
+        </Link>
       </div>
     );
   }
