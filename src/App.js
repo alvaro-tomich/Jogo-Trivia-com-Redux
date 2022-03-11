89925f0405008a0d6497ac664b1a14e4a011b4e7
Kamila Hydalgo <kamilahydalgo@gmail.com>
2022-03-11 18:05:17 -0300

    Requisito 12-tela feddback

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route exact path="/" component={ Login } />
         <Route path="/game" component={ Game } />
         <Route path="/settings" component={ Settings } />
+        <Route path="/feedback" component={ Feedback } />
       </Switch>
     </div>
   );
